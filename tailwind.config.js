6a1b49f0645a7ed4ae3c0a0aea7c2fe39aea5ff7
josevictormoura <josevictormouraa@gmail.com>
2024-12-19 23:24:44 -0300

    adcionar config costumizadas para home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        lemo: ['Lemon', 'cursive']
+      },
+      backgroundImage: {
+        "home": "url(/assets/img/main.jpg)",
+        "azul-dark": "#172554",
+        "azul": "#48cfff",
+        "gradient": "linear-gradient(to right, #b5ecff, #48cfff)",
+        "gradient-2": "linear-gradient(to bottom, #48cfff 5%, #fff 5%)"
+      },
+      colors: {
+        "azul": '#48cfff'
+      },
+      boxShadow: {
+        "sombra": "rgba(0, 0, 0, 0.24) 0px 3px 8px"
+      }
+    },
   },
   plugins: [],
 }
